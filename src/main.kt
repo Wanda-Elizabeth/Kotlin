cff7ed8cfb010b769e743a921f0df6c17116312a
WandaElizabeth <wandawairimu@gmail.com>
2022-03-16 08:53:41 +0300

    kotlin4

---
+++ src/main.kt
@@ -0,0 +1,38 @@
+fun main(){
+  var f=productintegers(arrayOf(48,3,59,5,5))
+    println(f)
+    var d=sumarray(arrayOf(7.1,65,3.3,657,"hyy",5,"wanjiru"))
+    println(d)
+    var s=numberofVowels(arrayOf('s','a','g','u','r','o'))
+    println(s)
+
+
+
+}
+fun productintegers(numbers:Array<Int>):Int{
+    var  product=1
+    numbers.forEach { number->
+      product*=number
+
+    }
+     return product
+}
+fun sumarray(mixed:Array<Any>):Double{
+    var sum=0.00
+    mixed.forEach { t->
+      if (t is Double||t is Float){
+        sum+=t.toString().toDouble()
+        }
+        }
+   return sum
+}
+fun numberofVowels(character:Array<Char>):Int{
+    var vowel=0
+    character.forEach { char->
+        if (char=='a'||char=='e'||char=='i'||char=='o'||char=='u'){
+
+        vowel++
+        }
+    }
+     return vowel
+}
